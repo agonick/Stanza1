891254630f0c9f568961fb7852a8fe08b0a9643f
agon <agostinonik@gmail.com>
2021-06-16 11:53:25 +0200

    Scheletro metodi sharing mobility

--- SharingMob1/src/SharingMobilitymain.java
+++ SharingMob1/src/SharingMobilitymain.java
@@ -22,6 +22,27 @@ public class SharingMobilitymain {
     return u.getCognome().charAt(0)+u.getCognome().charAt(1)+u.getNome().charAt(0)+u.getNome().charAt(1) + String.valueOf(getProgressivo());
     }
 
+    public void affittaVeicolo(Utente U, Veicolo V, int tempo){
+        // controlla se utente è registrato id != null
+        // controlla se veicolo è libero stato = DISPONIBILE
+        // controlla se utente ha credito minimo
+        // controlla se veicolo ha carburante minimo
+        // controlla tempo del noleggio
+        // crea un nuovo noleggio
+        // aggiunge a database noleggio
+        // aggiorna gli stati
+    }
+
+    public void restituisciVeicolo(Utente u, Veicolo v){
+        // togli noleggio da database noleggio
+        // aggiorna stato veicolo
+    }
+
+
+    
+
+
+
 
     
 
