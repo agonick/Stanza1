295c6b094a7b5efaa80d3edf17ae8b9a57888a88
Ruben <laroccaruben1@icloud.com>
2021-06-16 11:19:10 +0200

    Classe Noleggio

---
+++ SharingMob1/src/Noleggio.java
@@ -0,0 +1,48 @@
+package Noleggio;
+import  Database.Database;
+import Utente.Utente;
+import Veicoli.Veicolo;
+
+public class Noleggio {
+    private Utente utente ;
+
+    public Utente getUtente() {
+        return utente;
+    }
+
+    public Veicolo getVeicolo() {
+        return veicolo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setVeicolo(Veicolo veicolo) {
+        this.veicolo = veicolo;
+    }
+
+    public void setUtente(Utente utente) {
+        this.utente = utente;
+    }
+
+    private Veicolo veicolo;
+    private String id;
+    public  Noleggio (String id,Veicolo veicolo,Utente utente){
+        setId(id);
+        setUtente(utente);
+        setVeicolo(veicolo);
+
+
+    }
+
+
+
+
+
+
+}
