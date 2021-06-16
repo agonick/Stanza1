a43f89070542fb1c535c33f3fca5477a9af9f2e6
Michela <michel.davino@studenti.unina.it>
2021-06-16 11:40:39 +0200

    scooter/auto/furgoncini

---
+++ SharingMob1/src/Furgoncini.java
@@ -0,0 +1,7 @@
+public class Furgoncini extends  VeicoliConTarga{
+
+
+    public Furgoncini(String ID, String posGeo, StatoVeicolo stato, double prezzoXMin, boolean disponibile, String targa, Patente patente, int carburante) {
+        super(ID, posGeo, stato, prezzoXMin, disponibile, targa, patente, carburante);
+    }
+}
---
+++ SharingMob1/src/Scooter.java
@@ -0,0 +1,7 @@
+public class Scooter extends VeicoliConTarga{
+
+
+    public Scooter(String ID, String posGeo, StatoVeicolo stato, double prezzoXMin, boolean disponibile, String targa, Patente patente, int carburante) {
+        super(ID, posGeo, stato, prezzoXMin, disponibile, targa, patente, carburante);
+    }
+}
